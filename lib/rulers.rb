6e054ddb5b3f33b23f09e99444a7500930116387
Jordan Hudgens <jordan@wow.com>
2015-01-12 17:15:40 -0600

    integrated routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,8 +4,20 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
