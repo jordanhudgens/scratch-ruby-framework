c8112f390a854ccadd330ee37861f82a964ae740
Jordan Hudgens <jordan@wow.com>
2015-02-03 13:01:32 -0600

    added controller call to the view to render erb files

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -22,16 +23,6 @@ module Rulers
         puts "To the rescue!"
       end
       [200, {'Content-Type' => 'text/html'}, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
