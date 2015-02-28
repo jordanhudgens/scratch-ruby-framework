c7fad1652cce0a3b52b541b35fae5d4d58b435db
Jordan Hudgens <jordan@wow.com>
2015-02-27 19:31:13 -0600

    Implemented updates to handle rack/request calls

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -9,21 +9,19 @@ module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Type' => 'text/html'}, []]
-      end
-
-      if env['PATH_INFO'] == '/'
-        return [301, {'Content-Type' => 'text/html'}, ["This is the homepage"]]
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
       end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      begin
-        text = controller.send(act)
-      rescue
-        puts "To the rescue!"
+      text = controller.send(act)
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
       end
-      [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,11 +1,12 @@
 require "erubis"
 require "rulers/file_model"
+require "rack/request"
 
 module Rulers
   class Controller
     include Rulers::Model
-    
-    def initialize(evn)
+
+    def initialize(env)
       @env = env
     end
 
@@ -13,17 +14,40 @@ module Rulers
       @env
     end
 
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status=200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response # Only for Rulers
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
+
+    def render(view_name, locals = {})
+      filename = File.join "app", "views",
+        controller_name, "#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result locals.merge(:env => env)
+    end
+
     def controller_name
       klass = self.class
       klass = klass.to_s.gsub /Controller$/, ""
       Rulers.to_underscore klass
-    end
-
-    def render(view_name, locals = {})
-      filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
-      template = File.read filename
-      eruby = Erubis::Eruby.new(template)
-      eruby.result locals.merge(:env => env)
     end
   end
 end
